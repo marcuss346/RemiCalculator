deda944b16b4e610356abab9a9d0b7058c27f3fd
marcuss346 <89461220+marcuss346@users.noreply.github.com>
2024-11-18 20:06:53 +0100

    Innit commit

--- app/src/main/java/com/example/remicalculator/MainActivity.kt
+++ app/src/main/java/com/example/remicalculator/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             RemiCalculatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android12",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
