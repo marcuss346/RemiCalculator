2d462caca2c204598053df745f01231522c8d8b6
marcuss346 <89461220+marcuss346@users.noreply.github.com>
2024-11-19 10:17:12 +0100

    odstranjeni nepotrebni importi in pregledani warningi

--- app/src/main/java/com/example/remicalculator/ui/NewGameScreen.kt
+++ app/src/main/java/com/example/remicalculator/ui/NewGameScreen.kt
@@ -11,11 +11,9 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
